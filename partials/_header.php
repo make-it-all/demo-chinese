d20677efd8e4bf469bdef6264907535c75d4444a
zachnurc <zach.nurcombe@gmail.com>
2016-11-11 13:47:20 +0000

    altered sidebar as test

--- partials/_header.php
+++ partials/_header.php
@@ -5,15 +5,15 @@
   <div class="flex-spacer"></div>
   <div id="user_section">
     Signed in as Michael Boss -
-    <a href="session/logout.php">Log out</a>
+    <a href="session/logout.php">退出</a>
   </div>
   <div id="language_selector" class="dropdown_container">
-    <label for="language_dropdown_controller" class="dropdown_label">English</label>
+    <label for="language_dropdown_controller" class="dropdown_label">中文(简体)</label>
     <input type="checkbox" id="language_dropdown_controller" class="dropdown_controller" />
     <div class="dropdown_contents">
       <a href="https://make-it-all-demo.herokuapp.com">English</a>
-      <a href="https://make-it-all-demo-arabic.herokuapp.com">Arabic</a>
-      <a href="https://make-it-all-demo-german.herokuapp.com">German</a>
+      <a href="https://make-it-all-demo-arabic.herokuapp.com">العربية</a>
+      <a href="https://make-it-all-demo-german.herokuapp.com">Deutsch</a>
     </div>
   </div>
 </div>
--- partials/_sidebar.php
+++ partials/_sidebar.php
@@ -1,4 +1,5 @@
 <div id="sidebar">
+  <a href="/">Dashboard</a>
   <section>
     <h4>Problems</h4>
     <a href="/problems">All</a>
@@ -12,11 +13,5 @@
     <a href="/users#operators">Operators</a>
     <a href="/users#specialists">Specialists</a>
     <a href="/users#admins">Admins</a>
-  </section>
-  <section>
-    <h4>Third Section</h4>
-    <a href="#">Menu Item x</a>
-    <a href="#">Look at me</a>
-    <a href="#">Nothing here</a>
   </section>
 </div>
